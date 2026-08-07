faa890ba6c64dd38d79101d75109deee52f85f63
srijatodupunuri <112269058+srijatodupunuri@users.noreply.github.com>
2026-08-07 12:42:38 +0530

    ats_criteria.py
    Define ATS criteria with weights and skills.

---
+++ ats_criteria.py
@@ -0,0 +1,52 @@
+ATS_CRITERIA = {
+
+    "weights": {
+
+        "skills": 50,
+
+        "experience": 20,
+
+        "education": 10,
+
+        "projects": 10,
+
+        "debugging": 10
+    },
+
+    "mandatory_skills": [
+
+        "embedded c",
+
+        "c programming"
+    ],
+
+    "preferred_skills": [
+
+        "linux",
+
+        "device driver",
+
+        "python",
+
+        "automation",
+
+        "git",
+
+        "gerrit",
+
+        "jenkins"
+    ],
+
+    "debugging_keywords": [
+
+        "debugging",
+
+        "troubleshooting",
+
+        "bug fixing",
+
+        "root cause analysis",
+
+        "issue resolution"
+    ]
+}

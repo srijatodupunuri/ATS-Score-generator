e1c916185dfe23fd7abc255ee77cba71c74ec6b5
srijatodupunuri <112269058+srijatodupunuri@users.noreply.github.com>
2026-08-07 14:10:44 +0530

    add this to routes folder

---
+++ routes>about.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template
+
+about = Blueprint(
+    "about",
+    __name__
+)
+
+
+@about.route("/about")
+
+def about_page():
+
+    return render_template(
+        "about.html"
+    )
